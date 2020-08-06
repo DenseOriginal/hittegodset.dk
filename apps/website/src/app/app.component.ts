72f3cfb28767afb7365f2c550030e853dbedea05
Anders Kok <ak.kornerup@gmail.com>
2020-08-06 19:03:07 +0200

    initial commit

---
+++ apps/website/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hittegodset.dk-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'website';
+}
