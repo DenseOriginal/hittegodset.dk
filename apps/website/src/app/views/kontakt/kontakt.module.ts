09f03c9ccc8897e3968d9b8631f0f89cc85023de
Anders Kok <ak.kornerup@gmail.com>
2020-08-11 18:30:25 +0200

    Added contact page

--- apps/website/src/app/views/kontakt/kontakt.module.ts
+++ apps/website/src/app/views/kontakt/kontakt.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { KontaktRoutingModule } from './kontakt-routing.module';
 import { KontaktComponent } from './page/kontakt.component';
+import { ComponentsModule } from "../../shared/components/components.module";
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
   declarations: [KontaktComponent],
   imports: [
     CommonModule,
-    KontaktRoutingModule
+    KontaktRoutingModule,
+    ComponentsModule,
+    MatButtonModule
   ]
 })
 export class KontaktModule { }
