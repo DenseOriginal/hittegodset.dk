04dee06e5607c50d8632ff96f17f127c62898bc1
Anders Kok <ak.kornerup@gmail.com>
2020-08-11 17:20:31 +0200

    Removed componentsModule from import

--- apps/website/src/app/app.module.ts
+++ apps/website/src/app/app.module.ts
@@ -5,11 +5,10 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from "./app-routing.module";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
-import { ComponentsModule } from './shared/components/components.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, LayoutModule, AppRoutingModule, BrowserAnimationsModule, CoreModule, ComponentsModule],
+  imports: [BrowserModule, LayoutModule, AppRoutingModule, BrowserAnimationsModule, CoreModule],
   providers: [],
   bootstrap: [AppComponent],
 })
